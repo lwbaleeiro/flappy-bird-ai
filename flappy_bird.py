8b729107c0834e1711ad5b48cc0d67f6f937254f
lwbaleeiro <dev.baleeiro@gmail.com>
2024-05-19 23:12:31 -0300

    general game inplementation

--- flappy_bird.py
+++ flappy_bird.py
@@ -8,19 +8,21 @@ from pipe import Pipe
 WIDTH, HEIGHT = 550, 800
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
 
-def draw_game(window, bird, base, pipe):
+def draw_game(window, bird, base, pipes):
     window.blit(BG_IMG, (0,0))
     base.draw(window)
     bird.draw(window)
-    pipe.draw(window)
+    for pipe in pipes:
+        pipe.draw(window)
     pygame.display.update()
 
 def main():
+    score = 0
     window = pygame.display.set_mode((WIDTH, HEIGHT))
     run = True
-    bird = Bird(100, 50)
-    base = Base(600)
-    pipe = Pipe(400)
+    bird = Bird(230, 350)
+    base = Base(730)
+    pipes = [Pipe(600)]
     clock = pygame.time.Clock()
 
     while run:
@@ -30,7 +32,27 @@ def main():
                 run = False
 
         #bird.move()
-        draw_game(window, bird, base, pipe)
+        pipes_to_remove = []
+        add_pipe = False
+        for pipe in pipes:
+            if pipe.collide(bird):
+                pass
+            if pipe.x + pipe.img_top.get_width() < 0:
+                pipes_to_remove.append(pipe)
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+            pipe.move()
+
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(600))
+
+        for pipe in pipes_to_remove:
+            pipes.remove(pipe)
+
+        base.move()
+        draw_game(window, bird, base, pipes)
 
     pygame.quit()
     quit()
